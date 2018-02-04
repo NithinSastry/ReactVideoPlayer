5bbb07315867c2c2b555d513fb5dd4d94faffaeb
NithinSastry <nithin.sastry@outlook.com>
2018-02-05 02:07:09 +0530

    Throttled search functionality.

--- src/index.js
+++ src/index.js
@@ -10,33 +10,44 @@ const API_KEY = "AIzaSyBrFqlTEmlnQ4XQm18ZHIw7a8VUPLhWrc4";
 class App extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             videos: [],
-            selectedVideo: null
+            selectedVideo: null,
+            searchStarted: false
         };
 
         this.videoSearch("Facebook React");
     }
 
-    videoSearch(query) {
-        YTSearch({ key: API_KEY, query: query }, (videos) => {
-            
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             });
+            this.state.searchStarted = false;
         });
     }
+
+    throttleSearch(query) {
+        this.term = query;
+        if (!this.state.searchStarted) {
+            setTimeout(() => {
+                this.videoSearch(this.term);
+            }, 1000);
+            this.state.searchStarted = true;
+        }
+    }
+
 
     render() {
         return (
             <div>
-                <SearchBar onSearch = {(query)=>{this.videoSearch(query)}} />
+                <SearchBar onSearch={(query) => {this.throttleSearch(query) }} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     videos={this.state.videos}
-                    onVideoSelect={(selectedVideo) => {this.setState({ selectedVideo })}}
+                    onVideoSelect={(selectedVideo) => { this.setState({ selectedVideo }) }}
                 />
             </div>
         );
