86d06c147917c7c750e1865c8487454c16d69b81
NithinSastry <nithin.sastry@outlook.com>
2018-02-05 00:28:30 +0530

    Video selection functionality

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,17 @@
-import React  from "react";
+import React from "react";
 import VideoListItem from "./video_list_item";
 
-const VideoList = (props)=>{
-    const videoItems = props.videos.map((video)=>{
-        return <VideoListItem key={video.etag} video={video}/>
+const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return (
+            <VideoListItem
+                key={video.etag}
+                video={video}
+                onVideoSelect={props.onVideoSelect}
+            />
+        )
     });
-    return(
+    return (
         <ul className="col-md-4 list-group">
             {videoItems}
         </ul>
--- src/index.js
+++ src/index.js
@@ -12,19 +12,26 @@ class App extends Component {
         super(props);
 
         this.state = {
-            videos: []
+            videos: [],
+            selectedVideo: null
         };
 
         YTSearch({ key: API_KEY, term: "pixel 2 xl" }, (videos) => {
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
     render() {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    videos={this.state.videos}
+                    onVideoSelect={(selectedVideo) => {this.setState({ selectedVideo })}}
+                />
             </div>
         );
     }
